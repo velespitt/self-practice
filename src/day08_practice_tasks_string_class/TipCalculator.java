8191558db5f5dfa1e61f1b8b9bdd6c3064141691
velespitt <yusuf.asik123@gmail.com>
2024-11-23 15:10:47 +0300

    Week 08 Practice Done

--- src/day08_practice_tasks_string_class/TipCalculator.java
+++ src/day08_practice_tasks_string_class/TipCalculator.java
@@ -1,12 +1,10 @@
 package day08_practice_tasks_string_class;
 
+import java.util.Scanner;
+
 public class TipCalculator {
     public static void main(String[] args) {
         /*
-        7. Create a class called TipCalculator and write a program for a tip calculator. There will be different service
-quality benchmarks that will determine the tip given. There will also be the ability to calculate based on the
-number of people in the party and whether there is a split of the bill.
-
         Poor = 5%, Fair = 10%, Good = 15%, Great = 20%, Excellent = 25%
 
       The program should ask the user to enter:
@@ -35,5 +33,60 @@ number of people in the party and whether there is a split of the bill.
                  Tip per person: 29.75
 
          */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Split or No Split (Yes or No)?");
+        String split = scanner.next();
+
+        System.out.println("Enter the number of people:");
+        int numberOfPeople = scanner.nextInt();
+
+        System.out.println("Enter the check amount:");
+        double check = scanner.nextDouble();
+
+        System.out.println("How was the service quality?");
+        String service = scanner.next();
+
+        scanner.close();
+
+        double totalTip = 0;
+        double perPerson = 0;
+        double totalPay = 0;
+        double tipPerPerson = 0;
+
+        // Service quality kontrolü
+        if (service.equals("Excellent")) {
+            totalTip = check * 0.25;
+        } else if (service.equals("Great")) {
+            totalTip = check * 0.20;
+        } else if (service.equals("Good")) {
+            totalTip = check * 0.15;
+        }
+        else if (service.equals("Fair")) {
+            totalTip = check * 0.10;
+        }
+        else if (service.equals("Poor")) {
+            totalTip = check * 0.5;
+        }
+
+        // Total pay, tip hesaplandıktan sonra güncellenmeli
+        totalPay = totalTip + check;
+
+        if (split.equalsIgnoreCase("Yes")) {
+            perPerson = totalPay / numberOfPeople;
+            tipPerPerson = totalTip / numberOfPeople;
+
+
+            System.out.println("Number of people entered: " + numberOfPeople);
+            System.out.println("Total to pay: " + totalPay);
+            System.out.println("Total tip: " + totalTip);
+            System.out.println("Total per person: " + perPerson);
+            System.out.println("Tip per person: " + tipPerPerson);
+        } else {
+            System.out.println("Number of people entered: " + numberOfPeople);
+            System.out.println("Total to pay: " + totalPay);
+            System.out.println("Total tip: " + totalTip);
+        }
+
     }
 }
